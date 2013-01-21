4bb3d825ef31b25f987ea6682e9b9db9b6bc7b1e
Taylor McGann <taylor@taylormcgann.com>
2013-01-20 19:30:16 -0700

    Starting into basic storyboarding and classing.

--- CityApp/CPDirectoryTVC.h
+++ CityApp/CPDirectoryTVC.h
@@ -1,13 +1,13 @@
 //
-//  CPViewController.h
+//  CPDirectoryTVC.h
 //  CityApp
 //
-//  Created by Taylor McGann on 1/14/13.
+//  Created by Taylor McGann on 1/20/13.
 //  Copyright (c) 2013 Taylor McGann. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CPViewController : UIViewController
+@interface CPDirectoryTVC : UITableViewController
 
 @end
---
+++ CityApp/CPDirectoryTVC.m
@@ -0,0 +1,75 @@
+//
+//  CPDirectoryTVC.m
+//  CityApp
+//
+//  Created by Taylor McGann on 1/20/13.
+//  Copyright (c) 2013 Taylor McGann. All rights reserved.
+//
+
+#import "CPDirectoryTVC.h"
+
+#define NUMBER_OF_SECTIONS 1;
+
+@interface CPDirectoryTVC ()
+
+@end
+
+@implementation CPDirectoryTVC
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return NUMBER_OF_SECTIONS;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
